822c5172256160743e3a8ec3c77512e07295b7dc
wmgcat <twiircl@gmail.com>
2022-10-30 12:18:06 +0300

    fix {}

--- 2.2/main.cpp
+++ 2.2/main.cpp
@@ -6,8 +6,12 @@ int main() {
   scanf("%i", &a);
   printf("b = ");
   scanf("%i", &b);
-  if (a > b) printf("a is more that b");
-  else {
+  if (a > b)
+  {
+    printf("a is more that b");
+  }
+  else
+  {
     if (a == b) printf("a = b");
     else printf("a is less that b");
   }
