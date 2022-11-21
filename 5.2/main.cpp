19a304185e4a1b002deb5c1e6c0f1a32cf8c705e
wmgcat <twiircl@gmail.com>
2022-11-21 10:34:07 +0300

    added 5.2 homework

---
+++ 5.2/main.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+  // input:
+  int cols, rows;
+  printf("enter cols: ");
+  scanf("%i", &cols);
+  printf("enter rows: ");
+  scanf("%i", &rows);
+  if (rows <= 0 || cols <= 0) {
+    printf("Error!");
+    return 0;
+  }
+  int arr[cols][rows];
+  for (int i = 0; i < cols; i++) {
+    for (int j = 0; j < rows; j++) {
+      printf("enter %i, %i array value: ", i + 1, j + 1);
+      scanf("%i", &arr[i][j]);
+    }
+  }
+  
+  // output:
+  printf("output:\n\n");
+  for (int i = 0; i < cols; i++) {
+    for (int j = 0; j < rows - 1; j++) {
+      printf("%i ", arr[i][j]);
+    }
+    printf("%i\n", arr[i][rows - 1]);
+  }
+}
