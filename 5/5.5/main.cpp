e9fff00b66c62442b922ef5b2722860b255f30d8
wmgcat <twiircl@gmail.com>
2022-11-26 13:13:37 +0300

    added 5.5 homework

---
+++ 5/5.5/main.cpp
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  // input:
+  int cols, rows;
+  printf("enter cols: ");
+  scanf("%i", &cols);
+  printf("enter rows: ");
+  scanf("%i", &rows);
+  if (rows <= 0 || cols <= 0) {
+    printf("Error!");
+    return 0;
+  }
+  int **arr = new int*[cols];
+  for (int i = 0; i < cols; i++) {
+    arr[i] = new int[rows];
+    for (int j = 0; j < rows; j++) {
+      printf("enter %i, %i array value: ", i + 1, j + 1);
+      scanf("%i", &arr[i][j]);
+    }
+  }
+  
+  // invert: 
+  int **subarr = new int*[rows];
+  for (int i = 0; i < rows; i++) {
+    subarr[i] = new int[cols];
+    for (int j = 0; j < cols; j++) {
+      subarr[i][j] = arr[j][i];
+    }
+  }
+
+  // output:
+  printf("output:\n\n");
+  for (int i = 0; i < cols; i++) {
+    for (int j = 0; j < rows; j++) {
+      printf("%i ", arr[i][j]);
+    }
+    printf("\n");
+    // clear subarray for memory:
+    delete[] arr[i];
+  }
+  // clear array for memory:
+  delete[] arr;
+
+  printf("invert:\n\n");
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < cols; j++) {
+      printf("%i ", subarr[i][j]);
+    }
+    printf("\n");
+    // clear subarray for memory:
+    delete[] subarr[i];
+  }
+  // clear array for memory:
+  delete[] subarr;
+}
