b2b476435d0196e4ad87ac74ab113f3fd48c2204
wmgcat <twiircl@gmail.com>
2022-12-06 22:33:51 +0300

    added 7.3 homework

---
+++ 7/7.3/main.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+// function:
+int calc(int a, int b) {
+  if (a*a == b || b*b == a) {
+    return 1;
+  }
+  return 0;
+}
+int calc(int a, int b, int c) {
+  return calc(a, b) + calc(a, c) + calc(b, c);
+}
+void calc(int a, int b, int c, int *value) {
+  (*value) = calc(a, b, c);
+}
+void calc(int a, int b, int c, int &value) {
+  value = calc(a, b, c);
+}
+
+int main() {
+  int a, b, c, value = 0;
+  int *point = new int;
+  
+  // input:
+  printf("a: ");
+  scanf("%i", &a);
+  printf("b: ");
+  scanf("%i", &b);
+  printf("c: ");
+  scanf("%i", &c);
+
+  calc(4, 16, 2, point); // pointer
+  calc(7, 49, 221, &value); // link
+  
+  // output & clear:
+  printf("return value: %i\npointer: %i\nlink: %i\n", calc(a, b, c), *point, value);
+  delete point;
+}
