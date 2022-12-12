6f2e232852ceeb9365a20e2133d5928052b9f01d
wmgcat <twiircl@gmail.com>
2022-12-12 16:00:16 +0300

    fix 1 part

--- C/homework/8/8.1/1.cpp
+++ C/homework/8/8.1/1.cpp
@@ -3,93 +3,81 @@
 #include <string.h>
 
 using namespace std;
-class StudentCard {
-  public:
-    static StudentCard create(int _old) {
-      StudentCard st;
-      st.old = _old;
-      st.course = 1;
-      return st;
-    }
-    void setCourse(int my_old) {
-      int range = my_old - old;
-      if (range > 0) {
-        course = 1 + range;
-      }
-    }
-    int getCourse() { return course; }
-    void getInfo() {
-      printf("student card:\ncourse: %i\nold in start study: %i\n\n", course, old);
-    }
-  private:
-    int old, course;
-};
 
 class Person {
-  public:
-    Person(string _name, string _gender, int _old, int _mass) {
-      name = _name;
-      gender = _gender;
-      old = _old;
-      mass = _mass;
-      is_student = false;
-    } 
-    int getOld() { return old; }
-    int getMass() { return mass; }
-    string getName() { return name; }
-    string getGender() { return gender; }
-    void setOld(int newold) {
-      if (newold < old) {
-        printf("Error!\n%s can't be less years old than he is now!\n", name.c_str());
-      } else {
-        old = newold;
-        if (is_student) {
-          studentcard.setCourse(old);
-        }
-        printf("setting new old from %s: %i\n", name.c_str(), old);
-      }
-    }
-    void setMass(int newmass) {
-      mass = newmass;
-      printf("setting new mass from %s: %i\n", name.c_str(), mass);
+  public: 
+    Person() {}
+    Person(string Name, string Gender, int Old, int Mass) {
+      name = Name;
+      gender = Gender;
+      old = Old;
+      mass = Mass;
     }
     void getInfo() {
-      printf("info about %s:\ngender: %s\nold: %i\nmass: %ikg\n", name.c_str(), gender.c_str(), old, mass);
-      if (is_student) {
-        studentcard.getInfo();
-      }
+      printf("info by %s:\ngender: %s\nold: %i\tmass: %ikg\n", name.c_str(), gender.c_str(), old, mass);
     }
-    void setStudent() {
-      if (old >= 18 && !is_student) {
-        studentcard = StudentCard::create(old);
-        is_student = true;
-      } else {
-        if (old < 18) {
-          printf("Error!\n%s must be 18 years old or older to become a student!\n", name.c_str());
-        }
-        if (is_student) {
-          printf("Error!\n%s alredy is student!\n", name.c_str());
-        }
-      }
-    }
-  private:
+    int getOld() { return old; }
+    string getName() { return name; }
+    string getGender() { return gender; }
+    void setOld(int Old) { old = Old; }
+    void setName(string Name) { name = Name; }
+    void setGender(string Gender) { gender = Gender; }
+  protected:
     string name, gender;
-    int mass, old;
-    bool is_student;
-    StudentCard studentcard;
+    int old;
+  private:
+    int mass;
 };
-int main() {
-  Person maksim("Maksim", "male", 17, 75);
-  Person anna("Anna", "female", 29, 60);
 
-  maksim.setStudent();
-  anna.setStudent();
-  anna.setOld(30);
-  maksim.setOld(18);
-  maksim.setStudent();
-  maksim.setOld(17);
-  maksim.setOld(20);
-  anna.setStudent();
-  maksim.getInfo();
-  anna.getInfo();
+class Student : public Person {
+  public:
+    Student(string Name, string Gender, int Old) {
+      name = Name;
+      gender = Gender;
+      old = Old;
+      course = 1;
+    }
+    Student(string Name, string Gender, int Old, int Course) {
+      name = Name;
+      gender = Gender;
+      old = Old;
+      course = Course;
+    }
+    Student(Person person) {
+      name = person.getName();
+      gender = person.getGender();
+      old = person.getOld();
+      course = 1;
+    }
+    Student(Person person, int Course) {
+      name = person.getName();
+      gender = person.getGender();
+      old = person.getOld();
+      course = Course;
+    }
+    void getInfo() {
+      printf("info by student %s:\ngender: %s\nold: %i\tcourse: %i\n", name.c_str(), gender.c_str(), old, course);
+    }
+    void setCourse(int Course) { course = Course; }
+    int getCourse() { return course; }
+  private:
+    int course;
+};
+
+void setStudent(Person *person) {
+  if (person->getOld() >= 18) {
+    Student *stud = new Student(person->getName(), person->getGender(), person->getOld());
+    person = stud;
+  } else {
+    printf("Error! %s is not 18 or more!\n", person->getName().c_str());
+  }
+}
+
+int main() {
+  Person user1("Vasya", "male", 18, 30);
+  Student user2("Anna", "female", 19, 2);
+  Student user3(user1, 4);
+  user1.getInfo();
+  user2.getInfo();
+  user3.getInfo();
 }
