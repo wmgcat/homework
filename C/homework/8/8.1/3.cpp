eae59cb9dafac37d679b0cd409fa12db365e3b9e
wmgcat <twiircl@gmail.com>
2022-12-14 00:00:13 +0300

    3 part

--- C/homework/8/8.1/3.cpp
+++ C/homework/8/8.1/3.cpp
@@ -33,9 +33,47 @@ class Item {
       }
       return sum;
     }
+    string getInfo() { return id + "(x" + to_string(count) + ")"; }
+  protected:
+    string id;
   private:
     int max, count;
-    string id;
+};
+class Weapon : public Item {
+  public:
+    Weapon() {
+      max = 1;
+      id = "";
+      count = 0;
+      damage = 1;
+    }
+    Weapon(string Id, int Damage) {
+      id = Id;
+      damage = Damage;
+    }
+    string getInfo() {
+      return id + "(damage: " + to_string(damage) + ")";
+    }
+  private:
+    int count, max, damage;
+};
+class Armor : public Item {
+  public:
+    Armor() {
+      max = 1;
+      id = "";
+      count = 0;
+      armor = 1;
+    }
+    Armor(string Id, int Armor) {
+      id = Id;
+      armor = Armor;
+    }
+    string getInfo() {
+      return id + "(armor: " + to_string(armor) + ")";
+    }
+  private:
+    int count, max, armor;
 };
 
 class Inventory {
@@ -43,6 +81,8 @@ class Inventory {
     Inventory(int Max) {
       max = Max;
       items = new Item[Max];
+      armors = new Armor[Max];
+      weapons = new Weapon[Max];
     }
     void add(Item item, int count) {
       for (int i = 0; i < max; i++) {
@@ -61,33 +101,80 @@ class Inventory {
           }
           break;
         }
+      } 
+    }
+    void add(Weapon item, int count) {
+      for (int i = 0; i < max; i++) {
+        string slot_id = weapons[i].getId();
+        if ((item.getId() == slot_id || slot_id == "") && !weapons[i].isMax()) {
+          if (slot_id == "") {
+            weapons[i] = item;
+          }
+          int result = weapons[i].add(count);
+          if (result > 0) {
+            if (i < (max - 1)) {
+              add(item, result);
+            } else {
+              printf("Error, inventory is full!\n");
+            }
+          }
+          break;
+        }
       }
+    }
+    
+    int getNoEmpty() {
+      int count = 0;
+      for (int i = 0; i < max || count >= max; i++) {
+        if (items[i].getId() != "") count++;
+        if (weapons[i].getId() != "") count++;
+        if (armors[i].getId() != "") count++;
+      }
+      return count;
     }
     void info() {
       printf("inventory(%i):\n", max);
-      for (int i = 0; i < max; i++) {
+      int count = 0;
+      while (count < max) {
+        bool is_find = false;
+        for (int i = 0; i < max; i++) {
+          if (items[i].getId() != "") {
+            printf("%i. %s max: %i\n", ++count, items[i].getInfo().c_str(), items[i].getMax());
+            is_find = true;          
+          }
+        }
+
+        if (!is_find) printf("%i. empty\n", ++count);
+        
+      }
+      /*for (int i = 0; i < max; i++) {
         printf("%i. ", i + 1);
         string slot_id = items[i].getId();
         if (slot_id == "") {
           printf("empty\n");
         } else {
-          printf("%s x%i/%i\n", slot_id.c_str(), items[i].getCount(), items[i].getMax());
+          printf("%s max: %i\n", items[i].getInfo().c_str(), items[i].getMax());
         }
-      }
+      }*/
     }
   private:
     int max;
     Item *items;
+    Weapon *weapons;
+    Armor *armors;
 };
 
 int main() {
   Inventory inv(10);
   Item apple("apple", 8);
   Item wood("wood", 16);
-  
+  Weapon sword_wood("sword_wood", 2);
+  Weapon sword_golden("sword_golden", 5);  
+  Armor helmet_iron("helmet_iron", 3);
+
   inv.add(apple, 22);
-  inv.add(wood, 62);
-  inv.add(apple, 12);
-  inv.add(wood, 15);
+  inv.add(wood, 17);
+  inv.add(sword_wood, 2);
+  inv.add(helmet_iron, 1);
   inv.info();  
 }
