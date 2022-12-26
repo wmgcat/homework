4c35328c1a3b4e26afe444b3de4930a0ceddb040
wmgcat <twiircl@gmail.com>
2022-12-26 21:34:08 +0300

    added part 2

--- C/projects/9.1/main.cpp
+++ C/projects/9.1/main.cpp
@@ -27,6 +27,28 @@ void getData(float range, float proc) {
   }
   printf("------------------\n\t%f\t\t\t%f\n", pay_sum, proc_sum);
 }
+void getData(float range, float proc, int end) {
+  float pay = (range + getProcent(range, proc)) / time;
+  printf("month\tpay\t\tresult\t\tprocent\n");
+  float proc_sum = 0, pay_sum = 0;
+  for (int i = 0; i < end; i++) {
+    float nproc, npay = pay;
+    if (i != end - 1) {
+      nproc = getProcent((range + getProcent(range, proc)) / time, proc);
+      range -= pay;
+      pay_sum += pay + nproc;
+      proc_sum += nproc;
+    } else {
+      nproc = getProcent((range + getProcent(range, proc)) / time, proc);
+      pay_sum += range + nproc;
+      proc_sum += nproc;
+      npay = range;
+      range -= range;
+    }
+    printf("%i\t%f\t%f\t%f\n", i + 1, npay, range, nproc);
+  }
+  printf("------------------\n\t%f\t\t\t%f\n", pay_sum, proc_sum);
+}
 
 int main() {
   try {
@@ -43,8 +65,15 @@ int main() {
     // output:
     if (proc < 0 || start < 0 || end < start) throw std::overflow_error("input data is not correct!");    
     float range = end - start;
-    if (range > 0) getData(range, proc);
-    else throw std::overflow_error("delta <= 0");
+    if (range > 0) {
+      getData(range, proc);
+      int month;
+      printf("finish credit int month(2-%i): ", time);
+      scanf("%i", &month);
+      if (month < 2 || month > time) throw std::overflow_error("month < 2 or month > 12");
+      printf("\nfinish credit int %i month matrix:\n", month);
+      getData(range, proc, month);
+    } else throw std::overflow_error("delta <= 0");
   }
   catch(std::overflow_error &err) { // error messages:
     printf("Error!\nMsg: %s\n", err.what());
