20a4a8a58c1588067521a6b4919d536a22de80a6
Мажик <twiircl@gmail.com>
2023-04-29 23:54:03 +0300

    fix 2.4

--- Python/2.4/1.py
+++ Python/2.4/1.py
@@ -1,2 +1,3 @@
 a, b = int(input("a: ")), int(input("b: "))
 print("S:", a * b)
+print("P:", (a+b)*2)
--- Python/2.4/2.py
+++ Python/2.4/2.py
@@ -2,7 +2,7 @@ current = input("number: ")
 
 def getResult(number):
   if (len(number) < 5):
-    return false
+    return False
   return (pow(int(number[-2]), int(number[-1])) * int(number[-3])) / (int(number[0]) - int(number[1]))
 
 print("Result:", getResult(current))
