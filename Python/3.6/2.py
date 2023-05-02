628d53f57e9d53d715c3eeeaff9af587b04ccc4c
Мажик <twiircl@gmail.com>
2023-05-02 10:24:53 +0300

    fix

--- Python/3.6/2.py
+++ Python/3.6/2.py
@@ -1,5 +1,7 @@
-X, count = int(input("X: ")), 2
-for i in range(2, int(X / 2) + 1):
-  if X % i == 0: count += 1
+X, count = int(input("X: ")), 0
+if X > 0:
+  count = 1 + int(X > 1)
+  for i in range(2, int(X / 2) + 1):
+    if X % i == 0: count += 1
 
 print("Result: ", count)
